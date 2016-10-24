a7a05d51378b9e8907bf5e7decbb9bc7162a8f95
Raymond Torres <rt275@afsconnect2.njit.edu>
2016-10-24 01:20:48 -0400

    commited

--- table.php
+++ table.php
@@ -1,5 +1,4 @@
 <?php
-
 class html {
   protected $html;
 
@@ -14,27 +13,40 @@ class HTMLTable extends html {
   public function getTable($arry) {
 
     $this->html = '<table>' . '<tbody>';
-    for($row = 0; $row < count($arry); $row++) {
+
+    //first loop is for headings.
+      $this->html = $this->html . '<tr>';
+      for($col = 0; $col < count($arry[0]); $col++) {
+        $this->html = $this->html . '<th>' . $arry[0][$col] . '</th>';
+      }
+      $this->html = $this->html . '</tr>';
+
+    for($row = 1; $row < count($arry); $row++) {
       $this->html = $this->html . '<tr>';
       for($col = 0; $col < count($arry[$row]); $col++) {
         $this->html = $this->html . '<td>' . $arry[$row][$col] . '</td>';
       }
       $this->html = $this->html . '</tr>';
     }
-    $this->html = $this->html . '</tbody>';
+    $this->html = $this->html . '</tbody>' . '</table>';
+
   return $this->html;
   }
 }
 
 //$obj = new HTMLTable;
-$cars = array(
-  array(2016, 'Honda', 'Civic'),
-  array(2013, 'Subaru', 'Brz'),
-  array(2001, 'BMW', 'M5'),
-  array(2009, 'chevy', 'corvette')
-);
-$obj = new HTMLTable;
-echo $obj->getTable($cars);
+
+
+// $cars = array(
+//   array(2016, 'Honda', 'Civic'),
+//   array(2013, 'Subaru', 'Brz'),
+//   array(2001, 'BMW', 'M5'),
+//   array(2009, 'chevy', 'corvette')
+// );
+// $obj = new HTMLTable;
+// echo $obj->getTable($cars);
+
+
 // echo '<table>';
 // echo '<tbody>';
 // for($row = 0; $row < count($cars); $row++) {
