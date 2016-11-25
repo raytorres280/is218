b2029264e2e06a64f34ce2448d22d3ea3d3820d8
Raymond Torres <rt275@afsconnect1.njit.edu>
2016-11-25 01:51:01 -0500

    "worked"

--- csvreader.php
+++ csvreader.php
@@ -7,7 +7,24 @@ include 'table.php';
   while(!feof($myFile)) {
     $array_csv[] = fgetcsv($myFile);
   }
-  $obj = new HTMLTable;
-  echo $obj->getTable($array_csv);
-?>
 
+
+  $obj = new HTMLTable;
+
+  //echo filter_var($array_csv[0][0], FILTER_SANITIZE_STRING);
+  // for ($i = 0; i < count($array_csv); i++) {
+  //   for ($j = 0; j < count($array_csv[i]); j++) {
+  //     $array_csv[i][j] = filter_var($array_csv[i][j], FILTER_SANITIZE_STRING);
+  //   }
+  // }
+
+  foreach($array_csv as $arry) {
+    foreach($arry as $item) {
+      $item = filter_var($item, FILTER_SANITIZE_STRING);
+    }
+  }
+  echo $obj->getTable($array_csv);
+  //test for updates
+
+
+?>
